68c098ed860dd2fb12af356f5eb6f816bebfdf3b
Arne Van Daele (ItsDividerz) <arne.van.daele@icloud.com>
2020-09-17 03:10:49 +0200

    Implement config and fxmanifest

---
+++ config.lua
@@ -0,0 +1,10 @@
+---------------------------------------
+--    ESX_MOBILEMETH by Dividerz     --
+-- FOR SUPPORT: Arne#7777 on Discord --
+---------------------------------------
+
+Config = {}
+
+Config.requiredCops = 0
+Config.getVehicle = vector3(172.40, 252.95, 133.55)
+Config.spawnLocation = {x = 135.15, y = 275.92, z = 109.97, h = 336.22}
---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+---------------------------------------
+--    ESX_MOBILEMETH by Dividerz     --
+-- FOR SUPPORT: Arne#7777 on Discord --
+---------------------------------------
+
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Mobile Meth'
+author 'Dividerz (Arne)'
+
+version '1.0.0'
+
+server_scripts {
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'config.lua',
+	'client/main.lua'
+}
+
+dependencies {
+	'es_extended'
+}
